299df0f1f9042afe3364efd4897a0762f34a00f8
Ivonne Tournier <tournierivon@gmail.com>
2022-04-21 17:47:41 -0300

    segundo commit

---
+++ my-app/src/components/2Banner/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="bg-green-500 py-12 text-center">
+      <img src=''></img>
+    </div>
+  )
+}
+
+export default Banner
