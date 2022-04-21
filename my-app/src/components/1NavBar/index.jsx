0b51c8b6b7e9058fb73c1f40966a9470367f2150
Ivonne Tournier <tournierivon@gmail.com>
2022-04-21 17:48:05 -0300

    segundo commit

---
+++ my-app/src/components/1NavBar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="bg-blue-500">
+        <h1>Salón La Giralda By Ivonnet</h1>
+        <a target="blanc" href="">Nosotros</a>
+        <a target="blanc" href="">Sucursales</a>
+        <a target="blanc" href="">Servicios</a>
+        <a target="blanc" href="">Turnos</a>
+    </div>
+  )
+}
+
+export default NavBar
